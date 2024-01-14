5f5d89895191b34132328a69d3395575ae9b408d
bbeldycki <beldyckibartosz@gmail.com>
2024-01-14 23:38:28 +0100

    Finished Carlson functions.

--- Some Code/carlfunctions.cpp
+++ Some Code/carlfunctions.cpp
@@ -111,7 +111,7 @@ double_t CarlsonFunction::rdFunction(double_t x, double_t y, double_t z)
 		}
 		else if (std::max(x, y, z) > 4.5e25)
 		{
-			throw std::runtime_error("Error: One of the argument is to large. rdFunction.");
+			throw std::runtime_error("Error: One of the arguments is to large. rdFunction.");
 		}
 	}
 	catch (const std::exception& e)
@@ -138,5 +138,145 @@ double_t CarlsonFunction::rdFunction(double_t x, double_t y, double_t z)
 	result = c4 * ( -.30 / 14.0 + 9.0 * c4 - 45.0 * c5 * dZ / 260.0) + result;
 	result = f * result;
 	result = 3.0 * so + result / (ave * sqrt(ave));
+	return result;
+}
+
+void CarlsonFunction::iterationForRFFunction(double_t& x, double_t& y, double_t& z, double_t& dX, double_t& dY, double_t& dZ, double_t& ave)
+{
+	double_t alamb = 0;
+	alamb = sqrt(x * y) + sqrt(x * z) + sqrt(y * z);
+	x = 0.25 * (x + alamb);
+	y = 0.25 * (y + alamb);
+	z = 0.25 * (z + alamb);
+	ave = (x + y + z) / 3.0;
+	dX = (ave - x) / ave;
+	dY = (ave - y) / ave;
+	dZ = (ave - z) / ave;
+}
+
+double_t CarlsonFunction::rfFunction(double_t x, double_t y, double_t z)
+{
+	double_t dX, dY, dZ, ave, result;
+	double_t c2, c3;
+	try
+	{
+		if (std::min(x, y, z) < 0)
+		{
+			throw std::runtime_error("Error: One or more argument is lower than 0. rfFunction.");
+		}
+		else if (std::min(x + y, x + z, y + z) < 1.5e-38)
+		{
+			throw std::runtime_error("Error: Sum of any pair ex (x + y) is to close to 0. rfFunction.");
+		}
+		else if (std::max(x, y, z) > 3e37)
+		{
+			throw std::runtime_error("Error: One of the arguments is to large. rfFunction.");
+		}
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << "Exception caught: " << e.what() << std::endl;
+		return 0;
+	}
+	dX = dY = dZ = ave = result = 0;
+	// First iteration
+	iterationForRFFunction(x, y, z, dX, dY, dZ, ave);
+	while (std::max(fabs(x), fabs(y), fabs(z)) > 0.08)
+	{
+		iterationForRFFunction(x, y, z, dX, dY, dZ, ave);
+	}
+	c2 = dX * dY - pow(dZ, 2);
+	c3 = dX * dY * dZ;
+	// Compute rcFunction
+	result = (1.0 + (c2 / 24.0 - 0.1 - 3.0 * c3 / 44.0) * (c2 + c3 / 14.0)) / sqrt(ave);
+	return result;
+}
+
+void CarlsonFunction::iterationForRJFunction(double_t& x, double_t& y, double_t& z, double_t& dX, double_t& dY, double_t& dZ, double_t& dP, double_t& ave, double_t& so, double_t& f, double_t& p)
+{
+	double_t alamb, alfa, beta = 0;
+	alamb = sqrt(x * y) + sqrt(x * z) + sqrt(y * z);
+	alfa = pow((p * (sqrt(x) + sqrt(y) + sqrt(z)) + sqrt(x) * sqrt(y) * sqrt(z)), 2);
+	beta = p * pow(p * alamb, 2);
+	so = so + f * rcFunction(alfa, beta);
+	f = 0.25 * f;
+	x = 0.25 * (x + alamb);
+	y = 0.25 * (y + alamb);
+	z = 0.25 * (z + alamb);
+	p = 0.25 * (p + alamb);
+	ave = 0.2 * (x + y + z + 2.0 * p);
+	dX = (ave - x) / ave;
+	dY = (ave - y) / ave;
+	dZ = (ave - z) / ave;
+	dP = (ave - p) / ave;
+}
+
+double_t CarlsonFunction::rjFunction(const double_t& x, const double_t& y, const double_t& z, const double_t& p)
+{
+	double_t newX, newY, newZ, dX, dY, dZ, dP, ave, so, f, newP, result;
+	double_t a1, b1, rcx;
+	double_t c2, c3, c4, c5, c6;
+	try
+	{
+		if (std::min(x, y, z) < 0)
+		{
+			throw std::runtime_error("Error: One or more argument is lower than 0. rjFunction.");
+		}
+		else if (std::min(x + y, x + z, std::min(y + z, fabs(p))) < 2.5e-13)
+		{
+			throw std::runtime_error("Error: Sum of any pair ex (x + y) or abs(p) is to close to 0. rjFunction.");
+		}
+		else if (std::max(x, y, std::max(z, fabs(p))) > 9e11)
+		{
+			throw std::runtime_error("Error: One of the arguments is to large. rjFunction.");
+		}
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << "Exception caught: " << e.what() << std::endl;
+		return 0;
+	}
+	dX = dY = dZ = dP = ave = so = result = 0;
+	f = 1.0;
+	if (p > 0.0)
+	{
+		newX = x;
+		newY = y;
+		newZ = z;
+		newP = p;
+	}
+	else
+	{
+		newX = std::min(x, y, z);
+		newZ = std::max(x, y, z);
+		newY = x + y + z - newX - newZ;
+		a1 = 1.0 / (newY - p);
+		b1 = a1 * (newZ - newY) * (newY - newX);
+		newP = newY + b1;
+		rcx = rcFunction(newX * newZ / newY, p * newP / newY);
+	}
+	// First iteration
+	iterationForRJFunction(newX, newY, newZ, dX, dY, dZ, dP, ave, so, f, newP);
+	while (std::max(fabs(x), fabs(y), std::max(fabs(z), fabs(p))) > 0.05)
+	{
+		iterationForRJFunction(newX, newY, newZ, dX, dY, dZ, dP, ave, so, f, newP);
+	}
+	c2 = dX * dY + dX * dZ + dY * dZ;
+	c3 = dX * dY * dZ;
+	c4 = pow(dP, 2);
+	c5 = c2 - 3.0 * c4;
+	c6 = c3 + 2.0 * dP * (c2 - c4);
+	// Compute rcFunction
+	result = 3.0 * dP / 26.0 - 3.0 / 11.0;
+	result = (1.0 / 6.0 + dP * result) * c3;
+	result = result - dP * c4 / 3.0;
+	result = dP * c2 * (1.0 / 3.0 - 3.0 * dP / 22.0) + result;
+	result = 1.0 + c5 * (9.0 * c5 - 4.5 * c6 / 26.0 - 3.0 / 14.0) + result;
+	result = f * result;
+	result = 3.0 * so + result / (ave * sqrt(ave));
+	if (p <= 0.0)
+	{
+		result = a1 * (b1 * result + 3.0 * (rcx - rfFunction(newX, newY, newZ)));
+	}
 	return result;
 }
--- Some Code/carlfunctions.h
+++ Some Code/carlfunctions.h
@@ -8,8 +8,12 @@ class CarlsonFunction
 		~CarlsonFunction();
 		double_t rcFunction(const double_t& x, const double_t& y);
 		double_t rdFunction(double_t x, double_t y, double_t z);
+		double_t rfFunction(double_t x, double_t y, double_t z);
+		double_t rjFunction(const double_t& x, const double_t& y, const double_t& z, const double_t& p);
 
 	private:
 		void iterationForRCFunction(double_t& x, double_t& y, double_t& ave, double_t& sol);
 		void iterationForRDFunction(double_t& x, double_t& y, double_t& z, double_t& dX, double_t& dY, double_t& dZ, double_t& ave, double_t& sol, double_t& f);
+		void iterationForRFFunction(double_t& x, double_t& y, double_t& z, double_t& dX, double_t& dY, double_t& dZ, double_t& ave);
+		void iterationForRJFunction(double_t& x, double_t& y, double_t& z, double_t& dX, double_t& dY, double_t& dZ, double_t& dP, double_t& ave, double_t& so, double_t& f, double_t& p);
 };
